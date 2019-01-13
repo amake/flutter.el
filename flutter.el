f757f34d906134c2617f72d4d592f1ca6fee83e0
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-01-13 21:13:11 +0900

    Flesh out header

--- flutter.el
+++ flutter.el
@@ -1,6 +1,30 @@
 ;;; flutter.el --- Tools for working with Flutter SDK -*- lexical-binding: t -*-
 
+;; Copyright (C) 2018 Aaron Madlon-Kay
+
+;; Author: Aaron Madlon-Kay
+;; Version: 0.1.0
+;; URL: https://github.com/amake/flutter.el
+;; Package-Requires: ((emacs "24.5"))
+;; Keywords: languages
+
+;; This file is not part of GNU Emacs.
+
+;; flutter.el is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3, or (at your option) any later version.
+;;
+;; flutter.el is distributed in the hope that it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+;; A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; flutter.el.  If not, see http://www.gnu.org/licenses.
+
 ;;; Commentary:
+
+;; flutter.el is package for running the `flutter' binary from the Flutter SDK
+;; interactively.  It is most useful when paired with `dart-mode'.
 
 ;;; Code:
 

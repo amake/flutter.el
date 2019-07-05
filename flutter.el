b768c4ca127088408d7f21d32bc0bb917d2577b2
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-07-05 22:54:03 +0900

    Fix typo

--- flutter.el
+++ flutter.el
@@ -23,7 +23,7 @@
 
 ;;; Commentary:
 
-;; flutter.el is package for running the `flutter' binary from the Flutter SDK
+;; flutter.el is a package for running the `flutter' binary from the Flutter SDK
 ;; interactively.  It is most useful when paired with `dart-mode'.
 
 ;;; Code:

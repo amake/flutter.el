efa8deeff26e18f5bf1b49850db7ad5196c82699
storvik <petterstorvik@gmail.com>
2024-02-02 13:14:03 +0100

    Change flutter-args to flutter-run-args

--- flutter.el
+++ flutter.el
@@ -38,8 +38,8 @@
 (defvar flutter-sdk-path nil
   "Path to Flutter SDK.")
 
-(defvar flutter-args nil
-  "Space-delimited string of CLI flags passed to `flutter`.")
+(defvar flutter-run-args nil
+  "Space-delimited string of CLI flags passed to `flutter-run'.")
 
 
 ;;; Key bindings
@@ -262,9 +262,7 @@ args."
    (list (when current-prefix-arg
            (read-string "Args: "))))
   (flutter--with-run-proc
-   (if args
-       args
-     flutter-args)
+   (or args flutter-run-args)
    (display-buffer buffer)))
 
 (defun flutter--devices ()
